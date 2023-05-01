f1067509d3bb37cf84312ada8c5619f698be0562
Unnatirastogi27 <110484749+Unnatirastogi27@users.noreply.github.com>
2023-05-01 19:09:28 +0530

    Add files via upload

---
+++ inverted half pyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i,j,n,m ;
+  cin>>n;
+  cin>>m;
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=n-i+1;j++)
+    {
+      
+        cout<<"* ";
+      
+   
+    }
+     cout << endl;
+  }
+ 
+  return 0;
+}
